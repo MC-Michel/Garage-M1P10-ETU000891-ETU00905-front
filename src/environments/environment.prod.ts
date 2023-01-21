0634c4cbc2420d0c0a6890d5eec30485cf54a625
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-01-21 19:46:26 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,13 @@ export const environment = {
     inCirculation : 0, 
     deposited : 1,
     inReparation: 2
-  }
+  },
+  repairStatus : {
+    todo : 0,
+    inprogress : 1,
+    ended : 2
+  },
+  tva : 20,
   // baseUrl : "http://localhost:4200",
   // wsUrl : "http://localhost:3000",
 };
